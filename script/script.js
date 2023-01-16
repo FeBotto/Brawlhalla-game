965c9ff5d4818b53f8c932c24818709214cf51c7
Botto Federico <f.botto.2635@vallauri.edu>
2023-01-16 10:20:51 +0100

    First Commit

---
+++ script/script.js
@@ -0,0 +1,34 @@
+function show(){
+    document.getElementById("nav1").style.width = "300px";
+    document.getElementById("nav2").style.width = "300px";
+}
+
+function attacco1(){
+    let foto = "./img/attaccopersonaggio.png";
+    document.getElementById("prs").src = foto;
+}
+
+function difesa1(){
+    let foto = "./img/scudopersonaggio.png";
+    document.getElementById("prs").src = foto;
+}
+
+function schiva1(){
+    let foto = "./img/schivapersonaggio.png";
+    document.getElementById("prs").src = foto;
+}
+
+function attacco2(){
+    let foto = "./img/attaccopersonaggio2.png";
+    document.getElementById("prs1").src = foto;
+}
+
+function difesa2(){
+    let foto = "./img/scudopersonaggio2.png";
+    document.getElementById("prs1").src = foto;
+}
+
+function schiva2(){
+    let foto = "./img/schivapersonaggio2.png";
+    document.getElementById("prs1").src = foto;
+}
